1a895b88f6873e6af2971d165b9565e69b09c612
Michael Zaldarriaga <michael.zaldarriaga@gmail.com>
2017-12-09 09:22:29 -0600

    Michael Zaldarriaga : 2017-12-09 10:22 EST
    committing comment to add author
    committing

--- src/main/java/com/artilekt/bank/BankApplication.java
+++ src/main/java/com/artilekt/bank/BankApplication.java
@@ -1,5 +1,10 @@
 package com.artilekt.bank;
 
+/*
+ * @author    Vladimir Olenin
+ * @company   ROITraining
+ *
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
